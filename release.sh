b3004ab0754f909b80d30ada67869c48eddda295
Fredrik Medley <fredrik@meroton.com>
2024-04-15 00:18:44 +0200

    Use git tag as PyPi release version

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+
+git_tag=$(git describe --tags)
+pypi_version=$(echo "$git_tag" | sed 's/^v//')
+cp pyproject.toml pyproject.toml.bak
+sed -i -b 's/^version = "0\.0\.0"$/version = "'"${pypi_version}"'"/' pyproject.toml
+
+poetry build
+
+mv pyproject.toml.bak pyproject.toml
