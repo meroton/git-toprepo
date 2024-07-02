61542abaf5bd36c8bd393452cb27a2616a450e9d
lackofabettername <adrlarsson@gmail.com>
2024-07-02 23:30:07 +0200

    Partially ported MonoRepo

---
+++ src/cli.rs
@@ -0,0 +1,46 @@
+use clap::{Args, Parser, Subcommand};
+use std::env;
+
+///TODO: Program description
+#[derive(Parser, Debug)]
+#[command(version)]
+pub struct Cli {
+    #[arg(default_value_t = get_cwd())]
+    pub cwd: String,
+
+    #[command(subcommand)]
+    pub command: Commands,
+
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Init(Init),
+    Config,
+    Refilter,
+    Fetch(Fetch),
+    Push,
+}
+
+#[derive(Args, Debug)]
+pub struct Init {
+    repository: String,
+
+    directory: String,
+}
+
+#[derive(Args, Debug)]
+pub struct Fetch {
+    #[arg(long)]
+    skip_filter: bool,
+
+    remote: String,
+
+    #[arg(id="ref")]
+    reference: String,
+}
+
+
+fn get_cwd() -> String {
+    env::current_dir().unwrap().to_str().unwrap().to_string()
+}
