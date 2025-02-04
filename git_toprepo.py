bb444e296a5ff80da218ad7a8b49f5e6ad07b02f
Nils Wireklint <nils@meroton.com>
2025-02-04 16:27:26 +0100

    Revert "XXX warn for corrupt git-config porcelain"
    This reverts commit 937b5452dc7864767d16bd103812eed785604676.

--- git_toprepo.py
+++ git_toprepo.py
@@ -475,14 +475,8 @@ class ConfigDict(DefaultDict[str, List[str]]):
     def parse(config_lines: str) -> "ConfigDict":
         ret = ConfigDict()
         for line in config_lines.splitlines(keepends=False):
-            try:
-                key, value = line.split("=", 1)
-                ret[key].append(value)
-            except ValueError:
-                print("Warning: git config: could not parse config entry:", file=sys.stderr)
-                print(line, file=sys.stderr)
-                print("ignoring", file=sys.stderr)
-                pass
+            key, value = line.split("=", 1)
+            ret[key].append(value)
         return ret
 
     @staticmethod
