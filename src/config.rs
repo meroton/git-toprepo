4a2bdaf371c76776988abe7a5b0c404815608f4f
Fredrik Medley <fredrik@meroton.com>
2025-05-14 23:09:40 +0200

    Write reported errors to last-effective-git-toprepo.toml
    Sometimes, the errors can be hard to spot when there are too many
    warnings. Therefore, write the errors to disk.

--- src/config.rs
+++ src/config.rs
@@ -39,6 +39,9 @@ pub struct LogConfig {
     /// Warning messages that should be ignored and not displayed for the user.
     #[serde(default)]
     pub ignored_warnings: Vec<String>,
+    /// Error messages that were displayed to the user.
+    #[serde(skip_deserializing)]
+    pub reported_errors: Vec<String>,
     /// Warning messages that were displayed to the user.
     #[serde(skip_deserializing)]
     pub reported_warnings: Vec<String>,
