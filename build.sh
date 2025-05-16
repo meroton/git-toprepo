f61dc907da3dffad2483f2c3bf7dc3ac13f04651
Fredrik Medley <fredrik@meroton.com>
2025-05-16 21:30:04 +0200

    Refactor rustfmt handling

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-set -eux
+set -eu
+
+if [ $# -eq 1 ] && [ "$1" == "ci" ]; then
+  ci_mode=true
+elif [ $# -eq 0 ]; then
+  ci_mode=false
+else
+  echo "Usage: $0 [ci]"
+  echo "If 'ci' is passed, it will run in CI mode."
+  exit 2
+fi
+
+set -x
 
 # Run both with and without `--all-features` to make sure that both configurations work.
+if $ci_mode; then
+  cargo fmt --all --check
+else
+  # Normal users usually want the code to be formatted.
+  cargo fmt --all
+fi
 cargo clippy --all-targets --all-features -- -Dwarnings
 cargo clippy --all-targets -- -Dwarnings
 cargo build --all-targets --all-features
