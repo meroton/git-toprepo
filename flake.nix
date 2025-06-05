cf7e1f64c5d43429866c1c1bf4ee67fa1cbd892a
Albin Vass <github@albinvass.com>
2025-06-05 10:16:06 +0200

    Package git-toprepo with nix

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem = { config, pkgs, inputs', ... }: let
+        rustPlatform = let
+          toolchain = inputs'.fenix.packages.minimal.toolchain;
+        in pkgs.makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+        };
+        git-toprepo = let
+          # Needed to make the source path reproducible:
+          # https://nix.dev/guides/best-practices#reproducible-source-paths
+          src = builtins.path { path = ./.; name = "git-toprepo"; };
+        in rustPlatform.buildRustPackage {
+          inherit src;
+          pname = "git-toprepo";
+          version = "0.1.0";
+          cargoLock.lockFile = "${src}/Cargo.lock";
+          doCheck = false;  # TODO(albinvass): Fix tests in nix sandbox
+        };
+        in {
+          packages = {
+            inherit git-toprepo;
+            default =  git-toprepo;
+          };
+          apps = let
+              git-toprepo-app = {
+                type = "app";
+                program = "${git-toprepo}/bin/git-toprepo";
+              };
+            in {
+              inherit git-toprepo;
+              default = git-toprepo-app;
+            };
+        };
+    };
+}
