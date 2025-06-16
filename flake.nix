dd31e882bec46d716e5d53bd22eac3b3b88887fa
Albin Vass <github@albinvass.com>
2025-06-16 16:15:15 +0200

    Rely on passthru attributes for nix build version and timestamp
    This makes it easier to trace the commit using git-toprepo-unpatched
    in a nix derivation.

--- flake.nix
+++ flake.nix
@@ -49,20 +49,23 @@
                   git
               ];
               cargoLock.lockFile = "${src}/Cargo.lock";
+              passthru = {
+                inherit (self) lastModifiedDate;
+                rev = if (self ? rev) then self.rev else self.dirtyRev;
+              };
             };
           git-toprepo-patched = let
-              revision = if (self ? rev) then self.rev else self.dirtyRev;
-              timestamp = self.lastModifiedDate;
+              inherit (git-toprepo-unpatched.passthru) rev lastModifiedDate;
             in pkgs.runCommand "git-toprepo-patched" {
                   PATH = pkgs.lib.strings.makeSearchPath "bin" [
                     pkgs.gnused
                   ];
                 } ''
               mkdir -p $out/bin
-              newRev="$(printf "${revision}" | sed 's/^\(.\{34\}\).*-dirty$/\1-dirty/')"
+              newRev="$(printf "${rev}" | sed 's/^\(.\{34\}\).*-dirty$/\1-dirty/')"
               sed \
                 -e "s/${fakeRev}/$newRev/" \
-                -e "s/${fakeTimestamp}/${timestamp}/" \
+                -e "s/${fakeTimestamp}/${lastModifiedDate}/" \
               ${git-toprepo-unpatched}/bin/git-toprepo \
               > $out/bin/git-toprepo
               chmod +x $out/bin/git-toprepo
