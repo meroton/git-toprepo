201aa099f1f9d99cd8372dc6dbd2e809e1e65b4d
Nils Wireklint <nils@meroton.com>
2025-09-03 14:55:18 +0200

    Revert "NILS: Prepare to allow fetch of super's own ssh urls."
    This reverts commit 2c81477fb2589ce8e8653622500c3da6daa1b78e.

--- src/repo_name.rs
+++ src/repo_name.rs
@@ -79,19 +79,11 @@ impl FromStr for RepoName {
 #[derive(
     Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord, serde::Serialize, serde::Deserialize,
 )]
-// TODO: Reconsider the naming here. And that we want a rich `RepoName` for some
-// parts of the code base. But also just a type-safe string that can be either a
-// sub repo or the main repo.
-pub struct RepositoryName(pub String);
-
-#[derive(
-    Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord, serde::Serialize, serde::Deserialize,
-)]
-pub struct SubRepoName(RepositoryName);
+pub struct SubRepoName(String);
 
 impl SubRepoName {
     pub fn new(name: String) -> Self {
-        SubRepoName(RepositoryName(name))
+        SubRepoName(name)
     }
 }
 
@@ -99,7 +91,7 @@ impl Deref for SubRepoName {
     type Target = str;
 
     fn deref(&self) -> &Self::Target {
-        &self.0.0
+        &self.0
     }
 }
 
