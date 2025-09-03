5fee79b2d10c58d2f87b2d267187f699b4650266
Nils Wireklint <nils@meroton.com>
2025-09-03 14:55:07 +0200

    Assert that overrides are used

--- src/main.rs
+++ src/main.rs
@@ -331,11 +331,22 @@ fn checkout(_: &Cli, checkout: &cli::Checkout) -> Result<()> {
         http_server_override = Some(git_review.host);
     }
 
-    let git = Git::new();
     let parsed_remote = git_gr_lib::gerrit_project::parse_remote_url(&checkout.remote).unwrap();
+    // TODO: How should we ask for the username, or autodetect it?
+    // It is often missing from the remote! We could rely on `.gitreview`.
     let username_override = parsed_remote.username;
+
+    assert!(
+        username_override.is_some(),
+        "Username must be overridden, git-gr can't find it"
+    );
+    assert!(
+        http_server_override.is_some(),
+        "http server must be overridden, git-gr can't find it"
+    );
+    let git = Git::new();
     let gerrit = git.gerrit(
-        None,
+        /* gerrit remote name */ None,
         username_override,
         http_server_override,
         HTTPPasswordPolicy::Netrc,
